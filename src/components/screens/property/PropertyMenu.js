2d666695f699c783781e20bcba06b1e0a19e6102
Neto <ukpongneto@wellahealth.com>
2022-07-02 17:31:00 +0100

    form logic implemented

--- src/components/screens/property/PropertyMenu.js
+++ src/components/screens/property/PropertyMenu.js
@@ -2,35 +2,61 @@ import React, { useState } from "react";
 import { Tabs, Tab, Form, Row, Col, Button, Container } from "react-bootstrap";
 
 const MenuForm = () => {
+  const [location, setLocation] = useState("");
+  const [property, setProperty] = useState("");
+  const [bedroom, setBedroom] = useState(0);
+  const [budget, setBudget] = useState("");
+
+  const changeLocation = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const selectProperty = (event) => {
+    setProperty(event.target.value);
+  };
+
+  const selectBedroom = (event) => {
+    setBedroom(event.target.value);
+  };
+
+  const changeBudget = (event) => {
+    var str = event.target.value.toLocaleString("en-US");
+    setBudget(str);
+  };
+
   return (
     <div>
       <Form>
         <Row>
           <Col>
             <Form.Label>Location</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              type="text"
+              value={location}
+              onChange={changeLocation}
+            />
           </Col>
           <Col>
             <Form.Label>Property</Form.Label>
-            <Form.Select>
-              <option>House</option>
-              <option>Flat/Apartment</option>
-              <option>Office</option>
+            <Form.Select value={property} onChange={selectProperty}>
+              <option value="House">House</option>
+              <option value="Flat/Apartment">Flat/Apartment</option>
+              <option value="Office">Office</option>
             </Form.Select>
           </Col>
           <Col>
             <Form.Label>Bedroom</Form.Label>
-            <Form.Select>
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-              <option>5</option>
+            <Form.Select value={bedroom} onChange={selectBedroom}>
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
             </Form.Select>
           </Col>
           <Col>
             <Form.Label>Budget</Form.Label>
-            <Form.Control type="number" />
+            <Form.Control type="text" value={budget} onChange={changeBudget} />
           </Col>
           <Col>
             <Button className="mt-4" variant="dark">
