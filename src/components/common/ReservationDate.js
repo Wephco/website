3c64fde451a4f7edfd40c67fc51d10e670d6145d
Neto <ukpongneto@wellahealth.com>
2022-10-26 15:40:35 +0100

    refactored reservation date selection to its own component

---
+++ src/components/common/ReservationDate.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Calendar from "./Calendar";
+import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const ReservationDate = ({ title, route }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <i
+        style={{
+          textDecoration: "none",
+          color: "black",
+          fontWeight: "bold",
+        }}
+        onClick={() => navigate(-1)}
+        className="bi bi-arrow-left mr-3"
+      >
+        Back
+      </i>
+
+      <div className="header mt-5 text-center">
+        <h4
+          className="display-7"
+          style={{
+            fontWeight: "bold",
+          }}
+        >
+          {title}
+        </h4>
+      </div>
+      <Calendar route={route} />
+    </Container>
+  );
+};
+
+export default ReservationDate;
